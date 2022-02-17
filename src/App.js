2e3f5bc4ddc8e7a9f301ac4c27bcf062aa0e04a6
Vijay Biradar <72202814+iam-kira@users.noreply.github.com>
2022-02-17 10:01:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Route exact path='/' element={< StudentData />}></Route>
         <Route exact path='/all' element={< AllUsers />}></Route>
         <Route exact path='/add' element={< AddUsers />}></Route>
-        {/* <Route exact path='/edit/:id' element={< EditUser />}></Route> */}
+        <Route exact path='/edit/:id' element={< EditUser />}></Route>
       </Routes>
     </BrowserRouter>
   );
